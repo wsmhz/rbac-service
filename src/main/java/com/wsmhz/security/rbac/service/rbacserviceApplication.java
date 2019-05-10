037c163308e2c6cdfb017420a9c94b7b171241ca
TangBiJing <787740644@qq.com>
2019-05-10 16:23:33 +0800

    Create & Init rbac-service Project...

---
+++ src/main/java/com/wsmhz/security/rbac/service/rbacserviceApplication.java
@@ -0,0 +1,22 @@
+package com.wsmhz.security.rbac.service;
+
+import org.springframework.boot.SpringApplication;
+
+import com.wsmhz.common.business.annotation.WsmhzMicroServiceApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created By TangBiJing
+ * Description: 
+ */
+@EnableFeignClients("com.wsmhz")
+@ComponentScan("com.wsmhz.**")
+@SpringCloudApplication
+@WsmhzMicroServiceApplication
+public class RbacserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RbacserviceApplication.class, args);
+    }
+}
